0e55b6409cad206b51ac2d279515eae1c6b09167
https://loyalfoodmachine.com/ <77953005+bella123789@users.noreply.github.com>
2021-01-25 13:49:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,3 +15,9 @@ define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
+When you need precision machining and contract manufacturing services performed by expert food machinists, turn to Shandong Loyal Industrial Co.,Ltd.
+Provide complete mechanical solutions and project costing for a wide range of food machinery including batching, forming, drying (microwave drying ovens, etc.), baking, coating, frying, etc.
+All the equipment has been designed and produced independently, and has obtained many patents. We have successfully realized the extension, promotion and application of our products to breakfast cereals, various fried and non-fried snack foods, nutrition bars, biscuits, instant noodles and other fields.
+The company can provide food production equipment, process, formula development and application for a wide range of users. We are able to provide customers with complete project solutions, advice, planning, project design and engineering for food plants according to their specific requirements.
+Complete turnkey systems, centralized control systems, complete plant automation projects.
+Provide the installation, commissioning of machines and training of workers for customers.
